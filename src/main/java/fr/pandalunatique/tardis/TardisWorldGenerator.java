589e9cfecf37bda4083f4880cc0fb59003f50b3b
PandaLunatiquee <tristan.lecontedenis@gmail.com>
2022-04-03 17:25:39 +0100

    Starting

--- src/main/java/fr/pandalunatique/tardis/TardisWorldGenerator.java
+++ src/main/java/fr/pandalunatique/tardis/TardisWorldGenerator.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class TardisWorldGenerator extends ChunkGenerator {
 
     @Override
-    public ChunkData generateChunkData(World world, Random random, int x, int z, BiomeGrid biome) {
+    public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biomeGrid) {
         return createChunkData(world);
     }
 
