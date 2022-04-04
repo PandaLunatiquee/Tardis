542c1ea7ce13311b54813421fa04266d741e45fd
BuildTools <unconfigured@null.spigotmc.org>
2022-04-04 20:48:13 +0200

    Added ArtronCloud

--- src/main/java/fr/pandalunatique/tardis/TardisPlugin.java
+++ src/main/java/fr/pandalunatique/tardis/TardisPlugin.java
@@ -1,12 +1,15 @@
 package fr.pandalunatique.tardis;
 
+import fr.pandalunatique.tardis.artron.ArtronCloudLoop;
 import org.bukkit.Bukkit;
 import org.bukkit.WorldCreator;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.generator.ChunkGenerator;
+import fr.pandalunatique.tardis.artron.ArtronCloud;
 
 public class TardisPlugin extends JavaPlugin {
+
+    private static TardisPlugin instance;
 
 
     @Override
@@ -31,6 +34,16 @@ public class TardisPlugin extends JavaPlugin {
 
         // Connect to database and check if connection if valid, otherwise disable the plugin
         if(!Database.getInstance().canConnect()) Bukkit.getPluginManager().disablePlugin(this);
+        instance = this;
+
+        WorldCreator wc = new WorldCreator("Tardis");
+        wc.generator(new fr.pandalunatique.tardis.TardisWorldGenerator());
+        wc.createWorld();
+
+        //Arton system
+        getServer().getPluginManager().registerEvents(new ArtronCloud(), this);
+        ArtronCloudLoop acl = new ArtronCloudLoop();
+        acl.start();
 
     }
 
@@ -42,4 +55,7 @@ public class TardisPlugin extends JavaPlugin {
 
     }
 
+    public static TardisPlugin getInstance() {
+        return instance;
+    }
 }
