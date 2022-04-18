b53facc7ad3a123d132af3953f64bc67e41a82af
YohannCtrd <yohann.courtand.auditeur@lecnam.net>
2022-04-18 23:40:35 +0200

    Updated ModelID Updated TardisEntityType and sort them Added getModel() method in ItemBuilder

--- src/main/java/fr/pandalunatique/tardisplugin/entity/TardisEntityType.java
+++ src/main/java/fr/pandalunatique/tardisplugin/entity/TardisEntityType.java
@@ -1,17 +1,19 @@
 package fr.pandalunatique.tardisplugin.entity;
 
 public enum TardisEntityType {
-    DALEK,
-    THE_SILENCE,
     CYBERMAN,
-    CYBERSHADE,
     CYBERMITE,
-    THE_ENTITY,
-    VASHTA_NERADA,
-    SYCORAX,
-    STENZA,
-    TIME_LORD,
-    WEEPING_ANGEL,
+    CYBERSHADE,
+    DALEK,
+    JUDOON,
+    SNOWMAN,
     SONTARAN,
-    JUDOON
+    STENZA,
+    SYCORAX,
+    THE_ENTITY,
+    THE_SILENCE,
+    TIME_LORD,
+    VASHTA_NERADA,
+    VASHTA_NERADA_SPACESUIT,
+    WEEPING_ANGEL
 }
